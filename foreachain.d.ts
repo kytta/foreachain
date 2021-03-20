985a93db2f22d872a1f51f6fe0e6468f12dd9902
Nikita Karamov <nick@karamoff.dev>
2021-03-20 10:39:54 +0100

    Add type definitions

--- foreachain.d.ts
+++ foreachain.d.ts
@@ -1,21 +1,14 @@
-"use strict"
-
 /**
  * Performs the specified action for each element in a sequence, and returns it.
- * 
+ *
  * @param {(value: any, index: number, array: any[]) => void} callback
  * A function that accepts up to three arguments. forEach calls the callbackfn
  * function one time for each element in the sequence.
- * 
+ *
  * @param {any} [thisArg]
  * An object to which the this keyword can refer in the callbackfn function. If
  * thisArg is omitted, undefined is used as the this value.
- * 
+ *
  * @returns the initial sequence
  */
-function foreachain(callback, thisArg) {
-	this.forEach(callback, thisArg)
-	return this
-}
-
-module.exports = foreachain
+declare function foreachain(callback: (value: any, index: number, array: any[]) => void, thisArg?: any): any;
--- index.mjs
+++ index.mjs
@@ -1,4 +1,19 @@
-export default function (callback, thisArg) {
+/**
+ * Performs the specified action for each element in a sequence, and returns it.
+ * 
+ * @param {(value: any, index: number, array: any[]) => void} callback
+ * A function that accepts up to three arguments. forEach calls the callbackfn
+ * function one time for each element in the sequence.
+ * 
+ * @param {any} [thisArg]
+ * An object to which the this keyword can refer in the callbackfn function. If
+ * thisArg is omitted, undefined is used as the this value.
+ * 
+ * @returns the initial sequence
+ */
+function foreachain(callback, thisArg) {
 	this.forEach(callback, thisArg)
 	return this
 }
+
+export default foreachain
